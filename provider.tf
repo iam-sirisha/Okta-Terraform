7e4d71ea514b466f9cd53037bac6949f71df9d6d
iam-sirisha <167849090+iam-sirisha@users.noreply.github.com>
2025-12-22 17:47:53 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    okta = {
+      source  = "okta/okta"
+      version = "~> 6.5"
+    }
+  }
+}
+
+provider "okta" {
+  org_name  = var.okta_org_name
+  base_url  = var.okta_base_url
+  api_token = var.okta_api_token
+}
