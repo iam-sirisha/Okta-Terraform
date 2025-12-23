96238fda9f8d61a9969328812296469950bd5555
iam-sirisha <167849090+iam-sirisha@users.noreply.github.com>
2025-12-23 10:30:17 +0530

    Update main.tf with saml type

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+#################################
+#OIDC APPLICATIONS
+#################################
 module "oidc_apps" {
   source = "./modules/oidc_app"
 
@@ -14,4 +17,34 @@ module "oidc_apps" {
   consent_method            = each.value.consent_method
   issuer_mode               = each.value.issuer_mode
   refresh_token_rotation    = each.value.refresh_token_rotation
+}
+
+####################################
+#SAML APPLICATIONS
+####################################
+module "saml_apps" {
+  source = "./modules/saml_app"
+
+  for_each = var.saml_apps
+
+  label                    = each.value.label
+  sso_acs_url              = each.value.sso_acs_url
+  recipient                = each.value.recipient
+  destination              = each.value.destination
+  audience                 = each.value.audience
+  idp_issuer               = each.value.idp_issuer
+
+  subject_name_id_template = each.value.subject_name_id_template
+  subject_name_id_format   = each.value.subject_name_id_format
+
+  response_signed          = each.value.response_signed
+  assertion_signed         = each.value.assertion_signed
+
+  signature_algorithm      = each.value.signature_algorithm
+  digest_algorithm         = each.value.digest_algorithm
+
+  honor_force_authn        = each.value.honor_force_authn
+  authn_context_class_ref  = each.value.authn_context_class_ref
+
+  attribute_statements     = each.value.attribute_statements
 }
