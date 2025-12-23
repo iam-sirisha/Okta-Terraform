6e0721801814b9300bbefbf52a2a0553f16381f2
iam-sirisha <167849090+iam-sirisha@users.noreply.github.com>
2025-12-23 10:25:38 +0530

    Create main.tf in modules/saml_app/main.tf

---
+++ modules/saml_app/main.tf
@@ -0,0 +1,17 @@
+resource "okta_app_oidc" "this" {
+  label = var.label
+  type  = var.application_type
+
+  grant_types    = var.grant_types
+  response_types = var.response_types
+
+  redirect_uris             = var.redirect_uris
+  post_logout_redirect_uris = var.post_logout_redirect_uris
+
+  consent_method = var.consent_method
+  issuer_mode    = var.issuer_mode
+
+  refresh_token {
+    rotation_type = var.refresh_token_rotation
+  }
+}
